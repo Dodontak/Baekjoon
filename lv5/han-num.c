6918e165959f4aaa21653c6fe026e3d13dbd5241
전병민 <mu03019@naver.com>
2022-09-16 09:07:25 +0900

    updated han-num

--- lv5/han-num.c
+++ lv5/han-num.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+bool check_han_num(int n);
 
 int main(void)
 {
-	int n,a,b,c;
+	int n,count=0;
 	scanf("%d",&n);
-	if(n<100) printf("%d",n);
-	else if(n==1000) return 0;
-	else
+	for(int i=1;i<=n;i++)
 	{
-		a=n%10;
-		b=(n/10)%10;
-		c=(n/100)%10;
-		if(a-b==b-c) printf("%d",n);
-		else return 0;
+		if(check_han_num(i)==true)
+		{
+			count++;
+		}
 	}
+	printf("%d",count);
 	return 0;
+}
+
+bool check_han_num(int n)
+{
+	int a,b,c;
+        if(n<100) return true;
+        else if(n==1000) return false;
+        else
+        {
+                a=n%10;
+                b=(n/10)%10;
+                c=(n/100)%10;
+                if(a-b==b-c) return true;
+                else return false;
+        }
+        return false;
 }
